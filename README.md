fc1cc9c8232d1e84f0a4952f39572b25bda8a2ba
divsam <sammojaverian@gmail.com>
2024-12-19 10:37:17 -0500

    readme added + more args

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### Personal Finance Tracking From the Command Line Because... Why Not?
-Write a summary of a purchase, generates/updates a spreadsheet containing all your finances.
+Write a summary of a purchase, generates/updates a spreadsheet containing all your personal finances.
--- parser_utils.py
+++ parser_utils.py
@@ -5,6 +5,15 @@ def get_argument_parser():
     parser = argparse.ArgumentParser(
         description='Processing personal finance via the command line')
     parser.add_argument('--purchase', type=str,
-                        help='Add a purchase to the spreadsheet', required=True)
+                        help='Add a purchase to the spreadsheet', required=False)
+    parser.add_argument('--name', type=str, help='Name of the purchase',
+                        required=True)
+    parser.add_argument('--amount', type=float,
+                        help='Amount of the purchase', required=True)
+    parser.add_argument('--category', type=str,
+                        # todo this needs to be a part of a specific list
+                        help='Category of the purchase', required=True)
+    parser.add_argument('--date', type=str, help='Date of the purchase',
+                        required=False)  # todo this needs to be a specific format, default to the current date
 
     return parser
