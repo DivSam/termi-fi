6d353bcab6a8d709fde810320469325e80febfd1
divsam <sammojaverian@gmail.com>
2024-12-20 11:15:48 -0500

    slight change

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import argparse
 from parser_utils import get_argument_parser
 
 
 def main():
     args = get_argument_parser().parse_args()
-    print(f'Logging the purchase: {args.purchase}')
+    print(f'Logging the args: {args}')
 
 
 if __name__ == '__main__':
